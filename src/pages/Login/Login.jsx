755b7d11419ef15b9584b5dec607e06d3f22074b
Piyaporn <piyapornlahr@gmail.com>
2023-07-12 18:56:17 +0100

    Created authentication

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -4,6 +4,27 @@ import Header from "../../components/Header/Header";
 import LoginForm from "../../components/LoginForm/LoginForm";
 
 const Login = ({ setUser }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState(false);
+  const navigate = useNavigate();
+
+  const getUser = () => {
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        setUser(userCredential.user);
+        navigate("/weather");
+      })
+      .catch((error) => {
+        console.log(error);
+        setErrorMessage(!errorMessage);
+      });
+  };
+
+  const hideError = () => {
+    setErrorMessage(!errorMessage);
+  };
+
   return (
     <div className="login">
       <div>
@@ -17,24 +38,24 @@ const Login = ({ setUser }) => {
       </div>
       <div className="login__form">
         <LoginForm
-          // email={email}
-          // password={password}
-          // emailInput={setEmail}
-          // passwordInput={setPassword}
-          // hideError={hideError}
-          // getUser={getUser}
+          email={email}
+          password={password}
+          emailInput={setEmail}
+          passwordInput={setPassword}
+          hideError={hideError}
+          getUser={getUser}
         />
         <div>
-          {/* {errorMessage && ( */}
-          <div className="login__popUp-msg">
-            <h1 className="login__popUp-msg--text">
-              Please Enter a valid email and password!
-            </h1>
-            {/* <button className="login__popUp-msg--button" onClick={hideError}>
+          {errorMessage && (
+            <div className="login__popUp-msg">
+              <h1 className="login__popUp-msg--text">
+                Please Enter a valid email and password!
+              </h1>
+              <button className="login__popUp-msg--button" onClick={hideError}>
                 Try again
-              </button> */}
-          </div>
-          {/* )} */}
+              </button>
+            </div>
+          )}
         </div>
       </div>
     </div>
