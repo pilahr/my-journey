ff852172c8981cf6b85abf12e5ac4476bb309da3
Piyaporn <piyapornlahr@gmail.com>
2023-07-12 17:09:55 +0100

    Created login page with log in form

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,7 +6,7 @@ import { Link } from "react-router-dom";
 const Header = () => {
   return (
     <div className="heading">
-      <h2 className="heading__text">MY JOURNEY</h2>
+      <h3 className="heading__text">MY JOURNEY</h3>
       <div className="heading__links">
         <Link to="/">
           <h4 className="heading__links--text">HOME</h4>
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,11 +1,42 @@
 import React from "react";
 import "./Login.scss";
 import Header from "../../components/Header/Header";
+import LoginForm from "../../components/LoginForm/LoginForm";
 
-const Login = () => {
+const Login = ({ setUser }) => {
   return (
-    <div>
-      <Header />
+    <div className="login">
+      <div>
+        <Header />
+      </div>
+      <div>
+        <h2 className="login__text">Life is a journey.. </h2>
+        <h4 className="login__text">
+          Log In & Update your recent journey today.
+        </h4>
+      </div>
+      <div className="login__form">
+        <LoginForm
+          // email={email}
+          // password={password}
+          // emailInput={setEmail}
+          // passwordInput={setPassword}
+          // hideError={hideError}
+          // getUser={getUser}
+        />
+        <div>
+          {/* {errorMessage && ( */}
+          <div className="login__popUp-msg">
+            <h1 className="login__popUp-msg--text">
+              Please Enter a valid email and password!
+            </h1>
+            {/* <button className="login__popUp-msg--button" onClick={hideError}>
+                Try again
+              </button> */}
+          </div>
+          {/* )} */}
+        </div>
+      </div>
     </div>
   );
 };
