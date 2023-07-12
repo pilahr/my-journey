c346dd230efc3772cd19d10d601de983b3196ed1
Piyaporn <piyapornlahr@gmail.com>
2023-07-12 16:55:29 +0100

    Created header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import "./Header.scss";
+
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <div className="heading">
+      <h2 className="heading__text">MY JOURNEY</h2>
+      <div className="heading__links">
+        <Link to="/">
+          <h4 className="heading__links--text">HOME</h4>
+        </Link>
+        <Link to="/holidays">
+          <h4 className="heading__links--text">BLOG</h4>
+        </Link>
+        <Link to="/holiday/add">
+          <h4 className="heading__links--text">ADD</h4>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,8 +1,13 @@
 import React from "react";
 import "./Login.scss";
+import Header from "../../components/Header/Header";
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div>
+      <Header />
+    </div>
+  );
 };
 
 export default Login;
