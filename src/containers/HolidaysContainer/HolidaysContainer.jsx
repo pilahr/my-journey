1187affc164152c0cbc5bd1cc89a7bb612de0da7
Piyaporn <piyapornlahr@gmail.com>
2023-07-24 13:39:10 +0100

    Link added to holiday by id

--- src/containers/HolidaysContainer/HolidaysContainer.jsx
+++ src/containers/HolidaysContainer/HolidaysContainer.jsx
@@ -1,14 +1,21 @@
 import React from "react";
 import "./HolidaysContainer.scss";
 import HolidayCard from "../../components/HolidayCard/HolidayCard";
+import { Link } from "react-router-dom";
 
 const HolidaysContainer = ({ holidays }) => {
   return (
     <>
       {holidays.map((holiday, index) => (
-        <div className="holidays-container" key={index}>
-          <HolidayCard holiday={holiday} key={index} className="holidays-container__cards"/>
-        </div>
+        <Link key={index} to={`/holiday/${holiday._id}`}>
+          <div className="holidays-container" key={index}>
+            <HolidayCard
+              holiday={holiday}
+              key={index}
+              className="holidays-container__cards"
+            />
+          </div>
+        </Link>
       ))}
     </>
   );
